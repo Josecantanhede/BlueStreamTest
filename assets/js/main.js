052d77422eee188ec393f73f6290bb68c18e45d3
Jose Cantanhede Junior <cantanhede33@gmail.com>
2021-11-23 10:49:28 -0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,9 +2,7 @@
 
 $.when($.ready).then(() => {
     startScroller();
-    startTeammatesPicker();
     startClientsCarousel();
-    contactFormLoader();
 });
 
 let startScroller = () => {
@@ -13,39 +11,15 @@ let startScroller = () => {
         $([document.documentElement, document.body]).animate({
             scrollTop: $($(this).attr('href')).offset().top - NAVBAR_HEIGHT,
             easing: 'linear'
-        }, 300);
+        }, 800);
     });
-    
+
     $(CONTACT_NOW_BUTTON_ID).click(function (event) {
         event.preventDefault();
         $([document.documentElement, document.body]).animate({
             scrollTop: $($(this).attr('href')).offset().top - NAVBAR_HEIGHT,
             easing: 'linear'
         }, 300);
-    });
-};
-
-let startTeammatesPicker = () => {
-    teammatesListEl = $(TEAMMATES_LIST_ID);
-    teammatesDisplayerEl = $(TEAMMATES_DISPLAYER_ID);
-    teammatesItems = teammatesListEl.children();
-    teammates = teammatesDisplayerEl.children();
-    activeTeammate = teammatesDisplayerEl.find('.on-display').first();
-    teammatesItems.each(index => {
-        const teammateItemEl = $(teammatesItems[index]);
-        teammateItemEl.on('click', function () {
-            //Removing itens active class
-            teammatesItems.removeClass('on-display');
-            teammateItemEl.addClass('on-display');
-
-            activeTeammate.fadeOut(500, function () {
-                activeTeammate.removeClass('on-display');
-                activeTeammate = $(teammateItemEl.data('for'));
-                activeTeammate.addClass('on-display');
-                activeTeammate.fadeIn(500);
-
-            });
-        });
     });
 };
 
@@ -60,30 +34,6 @@ let startClientsCarousel = () => {
         fluidSpeed: 6000,
         autoplayHoverPause: true
     });
-};
-
-let contactFormLoader = () => {
-    contactFormValidation = $(CONTACT_FORM_ID).validate();
-
-    $(CONTACT_SUBMIT_BUTTON_ID).click(function () {        
-        $(CONTACT_FORM_ID).submit();
-    });
-    
-    $(CONTACT_FORM_ID).submit(function (event) {
-        event.preventDefault();
-        if ($(CONTACT_FORM_ID).valid()) {
-            const form = $(CONTACT_FORM_ID);            
-            const data = form.serialize();
-            $(CONTACT_SENDING_LOADER_ID).show();
-            $.post('php/mail.php', data).done(function() {
-                form.trigger("reset");
-                alert('Message sent');
-            }).always(function() {
-                $(CONTACT_SENDING_LOADER_ID).hide();
-            });;
-        }
-    });
-    
 };
 
 
@@ -104,7 +54,7 @@ const TEAMMATE_ITEM_CLASS = '.teammate-item';
 // Teammates Displayer
 const TEAMMATES_DISPLAYER_ID = '#teammates-displayer';
 
-// Contact Form 
+// Contact Form
 
 const CONTACT_FORM_ID = '#contact-form';
 const CONTACT_SUBMIT_BUTTON_ID = '#contactSubmitButton';
